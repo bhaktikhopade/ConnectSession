5715fbfe353385e474ab512f0847b40a8ea0d2dd
Bhakti Khopade <77563452+bhaktikhopade@users.noreply.github.com>
2021-06-23 18:48:02 +0530

    Add files via upload

---
+++ PalindromeString.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+
+void isPalindrome(char str[])
+{
+  int l=0;
+  int r=strlen(str)-1;
+  
+  while(r>1)
+  {
+    if(str[l++] != str[r--])
+    {
+      printf("%s is Not Palindrome",str);
+      return;
+    }
+  }
+  printf("%s is Palindrome",str);
+}
+
+int main()
+{
+  char str1[50];
+  while(1)
+  {
+      printf("\nEnter The String\n");
+      scanf("%s",str1);
+      isPalindrome(str1);
+  }
+  return 0;
+}
